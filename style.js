a96f7c2e04bfec3c81efa2b2c58cdacd43421357
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2024-03-26 18:55:54 +0530

    Initial commit

---
+++ style.js
@@ -0,0 +1,21 @@
+let btn=document.querySelector("button")
+let image=document.querySelector("img")
+let body=document.querySelector("body")
+let on=false;
+function lightOn(){
+    if(on==false){
+        btn.innerHTML="OFF"
+        image.src= "./assets/light-on.png";
+        image.src.boxShadow="0px 0px 20px #ffd500"
+        image.style.filter = `drop-shadow(0px 0px 30px #ffd500)`;
+        body.style.backgroundColor = "darkblue";
+        on=true;
+    }
+    else{
+        btn.innerHTML="ON"
+        image.src="./assets/light-off.png";
+        body.style.backgroundColor="#94acf1";
+        image.style.filter = `drop-shadow(0px 0px 30px black)`;
+        on=false;
+    }
+}
